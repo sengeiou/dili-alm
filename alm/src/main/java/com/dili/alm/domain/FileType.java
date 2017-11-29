42b817a2882c3195996f83a17b67f295cfdff984
DESKTOP-SQ9ASR4\jiang <jianglinhu@diligrp.com>
2017-11-29 11:29:24 +0800

    增加文件类型枚举类

---
+++ alm/src/main/java/com/dili/alm/domain/FileType.java
@@ -0,0 +1,23 @@
+package com.dili.alm.domain;
+
+public enum FileType {
+
+	PRODUCT("产品类文档", 1), DEVELOPMENT("开发类文档", 2), TEST("测试类文档", 3), PROJECT_INTRODUCE("项目介绍类文档", 4), ONLINE_INTRODUCE("上线部署说明文档", 5), ORTHER("其他文档", 0);
+
+	private String name;
+	private int value;
+
+	private FileType(String name, int value) {
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+}
