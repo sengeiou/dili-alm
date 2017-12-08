df9a0ab077b89e41cfaa4c71416e16d8a691672c
DESKTOP-SQ9ASR4\jiang <jianglinhu@diligrp.com>
2017-12-08 17:27:49 +0800

    解决冲突

---
+++ alm/src/main/java/com/dili/alm/domain/WorkScheduleEntity.java
@@ -0,0 +1,72 @@
+package com.dili.alm.domain;
+
+import com.dili.ss.domain.BaseDomain;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class WorkScheduleEntity extends BaseDomain implements WorkSchedule {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Date scheduleDate; 
+	
+	private String scheduleText;
+	 
+	private Long userId;
+	
+	private String scheduleDateStr;//将date日期转化为String 类型
+	
+
+	public WorkScheduleEntity(WorkSchedule entity) {
+		this.id = entity.getId();
+		this.scheduleDate = entity.getScheduleDate();
+		this.scheduleText = entity.getScheduleText();
+		this.userId = entity.getUserId();
+		this.scheduleDateStr = new SimpleDateFormat("yyyy-MM-dd").format(entity.getScheduleDate());
+	}
+
+	public String getScheduleDateStr() {
+		return scheduleDateStr;
+	}
+
+	public void setScheduleDateStr(String scheduleDateStr) {
+	  this.scheduleDateStr = scheduleDateStr;
+	}
+
+	@Override
+	public Date getScheduleDate() {
+		return scheduleDate;
+	}
+
+	@Override
+	public void setScheduleDate(Date scheduleDate) {
+		this.scheduleDate = scheduleDate;
+		
+	}
+
+	@Override
+	public String getScheduleText() {
+		return scheduleText;
+	}
+
+	@Override
+	public void setScheduleText(String scheduleText) {
+		this.scheduleText = scheduleText;
+	}
+
+	@Override
+	public Long getUserId() {
+		return userId;
+	}
+
+	@Override
+	public void setUserId(Long userId) {
+		userId = this.userId;
+	}
+
+
+}
---
+++ alm/src/main/java/com/dili/alm/utils/TypeUtil.java
@@ -0,0 +1,6 @@
+package com.dili.alm.utils;
+
+
+public class TypeUtil {
+	 
+}
