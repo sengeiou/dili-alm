3a843332678b5b6790fbefce8d4c98716a174868
sunguangqiang <sunguangqiang@nong12.com>
2018-01-05 17:56:56 +0800

    周报生成，返回按钮返回会到项目管理页面

--- alm/src/main/java/com/dili/alm/utils/DateUtil.java
+++ alm/src/main/java/com/dili/alm/utils/DateUtil.java
@@ -301,5 +301,14 @@ public static HashMap<String,String>  getFirstAndFive(Date date) {
 			return date;
 		}
 	}
-    
+	 public static String getWeekOfDay(Date dt) {
+	        String[] weekDays = {"星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"};
+	        Calendar cal = Calendar.getInstance();
+	        cal.setTime(dt);
+	        int w = cal.get(Calendar.DAY_OF_WEEK) - 1;
+	        if (w < 0)
+	            w = 0;
+	        return weekDays[w];
+	 }
+	
 }
