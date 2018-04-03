cbbb3bc1fcb8df09a35a4c72b3a3c37fc63b8080
hanjinzhao <hanjinzhao@nong12.com>
2018-04-03 14:01:00 +0800

    资源维护管理

---
+++ alm/src/main/java/com/dili/alm/controller/HardwareResourceController.java
@@ -0,0 +1,126 @@
+package com.dili.alm.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.dili.alm.domain.HardwareResource;
+import com.dili.alm.domain.Project;
+import com.dili.alm.domain.User;
+import com.dili.alm.service.HardwareResourceService;
+import com.dili.ss.domain.BaseOutput;
+import com.dili.ss.domain.EasyuiPageOutput;
+import com.dili.sysadmin.sdk.domain.UserTicket;
+import com.dili.sysadmin.sdk.session.SessionContext;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * 由MyBatis Generator工具自动生成
+ * This file was generated on 2018-03-20 17:22:08.
+ */
+@Api("/hardwareResource")
+@Controller
+@RequestMapping("/hardwareResource")
+public class HardwareResourceController {
+    @Autowired
+    HardwareResourceService hardwareResourceService;
+    private static final String DATA_AUTH_TYPE = "Project";
+
+    @ApiOperation("跳转到HardwareResource页面")
+    @RequestMapping(value="/index.html", method = RequestMethod.GET)
+    public String index(ModelMap modelMap) {
+        return "hardwareResource/index";
+    }
+
+    @ApiOperation(value="查询HardwareResource", notes = "查询HardwareResource，返回列表信息")
+    @ApiImplicitParams({
+		@ApiImplicitParam(name="HardwareResource", paramType="form", value = "HardwareResource的form信息", required = false, dataType = "string")
+	})
+    @RequestMapping(value="/list", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody List<HardwareResource> list(HardwareResource hardwareResource) {
+        return hardwareResourceService.list(hardwareResource);
+    }
+
+    @ApiOperation(value="分页查询HardwareResource", notes = "分页查询HardwareResource，返回easyui分页信息")
+    @ApiImplicitParams({
+		@ApiImplicitParam(name="HardwareResource", paramType="form", value = "HardwareResource的form信息", required = false, dataType = "string")
+	})
+    @RequestMapping(value="/listPage", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody String listPage(HardwareResource hardwareResource) throws Exception {
+    	@SuppressWarnings({ "rawtypes" })
+		List<Map> dataAuths = SessionContext.getSessionContext().dataAuth(DATA_AUTH_TYPE);
+		if (CollectionUtils.isEmpty(dataAuths)) {
+			return new EasyuiPageOutput(0, new ArrayList<>(0)).toString();
+		}
+		List<Long> projectIds = new ArrayList<>();
+        return hardwareResourceService.listEasyuiPageByExample(hardwareResource, projectIds, true).toString();
+    }
+
+    @ApiOperation("新增HardwareResource")
+    @ApiImplicitParams({
+		@ApiImplicitParam(name="HardwareResource", paramType="form", value = "HardwareResource的form信息", required = true, dataType = "string")
+	})
+    @RequestMapping(value="/insert", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody BaseOutput insert(List<HardwareResource> hardwareResources) {
+    	
+    	return hardwareResourceService.insertHardwareResourceSelective(hardwareResources);
+    }
+
+    @ApiOperation("修改HardwareResource")
+    @ApiImplicitParams({
+		@ApiImplicitParam(name="HardwareResource", paramType="form", value = "HardwareResource的form信息", required = true, dataType = "string")
+	})
+    @RequestMapping(value="/update", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody BaseOutput update(HardwareResource hardwareResource) {
+    	UserTicket userTicket = SessionContext.getSessionContext().getUserTicket();
+		if (userTicket == null) {
+			throw new RuntimeException("未登录");
+		}
+		hardwareResource.setMaintenanceOwner(userTicket.getId());
+		hardwareResource.setLastModifyDate(new Date());
+        hardwareResourceService.updateSelective(hardwareResource);
+        return BaseOutput.success("修改成功");
+    }
+
+    @ApiOperation("删除HardwareResource")
+    @ApiImplicitParams({
+		@ApiImplicitParam(name="id", paramType="form", value = "HardwareResource的主键", required = true, dataType = "long")
+	})
+    @RequestMapping(value="/delete", method = {RequestMethod.GET, RequestMethod.POST})
+    public @ResponseBody BaseOutput delete(Long id) {
+        hardwareResourceService.delete(id);
+        return BaseOutput.success("删除成功");
+    }
+    // 查询用户列表
+ 	@ResponseBody
+ 	@RequestMapping(value = "/listTreeMember.json", method = { RequestMethod.GET, RequestMethod.POST })
+ 	public List<User> listTreeMember() {
+ 		return hardwareResourceService.listUserByDepartment();
+ 	}
+ 	
+ 	// 查询项目列表
+  	@ResponseBody
+  	@RequestMapping(value = "/listProjectList.json", method = { RequestMethod.GET, RequestMethod.POST })
+  	public List<Project> listProjectList() {
+  		return hardwareResourceService.projectList();
+  	}
+  	//判断是否提交
+  	@ResponseBody
+	@RequestMapping(value = "/isOperation.json", method = { RequestMethod.GET, RequestMethod.POST })
+	public boolean isOperation(Long id) {
+		return hardwareResourceService.isOperation(id);
+	}
+}
---
+++ alm/src/main/java/com/dili/alm/domain/HardwareResource.java
@@ -0,0 +1,128 @@
+package com.dili.alm.domain;
+
+import com.dili.ss.dto.IBaseDomain;
+import com.dili.ss.metadata.FieldEditor;
+import com.dili.ss.metadata.annotation.EditMode;
+import com.dili.ss.metadata.annotation.FieldDef;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 由MyBatis Generator工具自动生成
+ * 
+ * This file was generated on 2018-04-03 12:02:09.
+ */
+@Table(name = "`hardware_resource`")
+public interface HardwareResource extends IBaseDomain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "`id`")
+    @FieldDef(label="id")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Long getId();
+
+    void setId(Long id);
+
+    @Column(name = "`project_id`")
+    @FieldDef(label="项目id")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Long getProjectId();
+
+    void setProjectId(Long projectId);
+
+    @Column(name = "`maintenance_date`")
+    @FieldDef(label="维护日期")
+    @EditMode(editor = FieldEditor.Datetime, required = true)
+    Date getMaintenanceDate();
+
+    void setMaintenanceDate(Date maintenanceDate);
+
+    @Column(name = "`machine_name`")
+    @FieldDef(label="机器名称", maxLength = 255)
+    @EditMode(editor = FieldEditor.Text, required = true)
+    String getMachineName();
+
+    void setMachineName(String machineName);
+
+    @Column(name = "`cpu_core_amount`")
+    @FieldDef(label="cpu内核数")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Integer getCpuCoreAmount();
+
+    void setCpuCoreAmount(Integer cpuCoreAmount);
+
+    @Column(name = "`memory`")
+    @FieldDef(label="内存")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Integer getMemory();
+
+    void setMemory(Integer memory);
+
+    @Column(name = "`disk`")
+    @FieldDef(label="磁盘")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Integer getDisk();
+
+    void setDisk(Integer disk);
+
+    @Column(name = "`ip`")
+    @FieldDef(label="ip地址", maxLength = 255)
+    @EditMode(editor = FieldEditor.Text, required = true)
+    String getIp();
+
+    void setIp(String ip);
+
+    @Column(name = "`regional`")
+    @FieldDef(label="地域")
+    @EditMode(editor = FieldEditor.Number, required = true)
+    Integer getRegional();
+
+    void setRegional(Integer regional);
+
+    @Column(name = "`note`")
+    @FieldDef(label="备注", maxLength = 255)
+    @EditMode(editor = FieldEditor.Text, required = false)
+    String getNote();
+
+    void setNote(String note);
+
+    @Column(name = "`is_submit`")
+    @FieldDef(label="是否已提交")
+    @EditMode(editor = FieldEditor.Number, required = false)
+    Integer getIsSubmit();
+
+    void setIsSubmit(Integer isSubmit);
+
+    @Column(name = "`maintenance_owner`")
+    @FieldDef(label="维护人员")
+    @EditMode(editor = FieldEditor.Number, required = false)
+    Long getMaintenanceOwner();
+
+    void setMaintenanceOwner(Long maintenanceOwner);
+
+    @Column(name = "`last_modify_date`")
+    @FieldDef(label="最后修改时间")
+    @EditMode(editor = FieldEditor.Datetime, required = false)
+    Date getLastModifyDate();
+
+    void setLastModifyDate(Date lastModifyDate);
+
+    @Column(name = "`service_environment`")
+    @FieldDef(label="使用环境（数据字典）")
+    @EditMode(editor = FieldEditor.Number, required = false)
+    Long getServiceEnvironment();
+
+    void setServiceEnvironment(Long serviceEnvironment);
+
+    @Column(name = "`cost`")
+    @FieldDef(label="成本")
+    @EditMode(editor = FieldEditor.Text, required = false)
+    BigDecimal getCost();
+
+    void setCost(BigDecimal cost);
+}
