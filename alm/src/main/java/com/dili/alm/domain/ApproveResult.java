7ec325d72fce72be5eb68deb4d581db3571471d2
DESKTOP-SQ9ASR4\jiang <jianglinhu@diligrp.com>
2018-04-09 14:24:10 +0800

    定义资源申请接口

---
+++ alm/src/main/java/com/dili/alm/domain/ApproveResult.java
@@ -0,0 +1,32 @@
+package com.dili.alm.domain;
+
+public enum ApproveResult {
+
+	APPROVED(1, "审核通过"), FAILED(0, "审核不通过");
+
+	private Integer value;
+	private String name;
+
+	private ApproveResult(Integer value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+
+	public static ApproveResult valueOf(Integer value) {
+		for (ApproveResult result : ApproveResult.values()) {
+			if (result.getValue().equals(value)) {
+				return result;
+			}
+		}
+		throw new IllegalArgumentException("未知的审核结果");
+	}
+
+	public Integer getValue() {
+		return value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
