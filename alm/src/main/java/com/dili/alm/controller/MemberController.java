f861a7db79de4ea8236a58a66dbef2f09dfaf20a
DESKTOP-SQ9ASR4\jiang <jianglinhu@diligrp.com>
2018-05-17 10:27:16 +0800

    人员工时统计页面控件优化

--- alm/src/main/java/com/dili/alm/controller/MemberController.java
+++ alm/src/main/java/com/dili/alm/controller/MemberController.java
@@ -1,8 +1,13 @@
 package com.dili.alm.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.dili.alm.cache.AlmCache;
 import com.dili.alm.domain.Department;
+import com.dili.alm.domain.User;
 import com.dili.alm.domain.dto.UserDepartmentRole;
 import com.dili.alm.domain.dto.UserDepartmentRoleQuery;
 import com.dili.alm.rpc.UserRpc;
@@ -50,5 +56,16 @@ public class MemberController {
 			return output.getData();
 		}
 		return null;
+	}
+
+	@ResponseBody
+	@RequestMapping("/loadUsersByDepartment")
+	public List<User> loadUsersByDepartment(@RequestParam(required = false) List<Long> departmentId) {
+		if (CollectionUtils.isEmpty(departmentId)) {
+			return new ArrayList<>(AlmCache.getInstance().getUserMap().values().stream()
+					.filter(u -> !u.getId().equals(1L)).collect(Collectors.toList()));
+		}
+		return AlmCache.getInstance().getUserMap().values().stream()
+				.filter(u -> departmentId.contains(u.getDepartmentId())).collect(Collectors.toList());
 	}
 }
