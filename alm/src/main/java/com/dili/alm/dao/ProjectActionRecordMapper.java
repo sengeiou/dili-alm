7ed31161206219658de4d6ce25091945f9a11b4a
DESKTOP-SQ9ASR4\jiang <jianglinhu@diligrp.com>
2018-07-12 09:56:24 +0800

    删除冲突文件

--- alm/src/main/java/com/dili/alm/dao/ProjectActionRecordMapper.java
+++
@@ -1,7 +0,0 @@
-package com.dili.alm.dao;
-
-import com.dili.alm.domain.ProjectActionRecord;
-import com.dili.ss.base.MyMapper;
-
-public interface ProjectActionRecordMapper extends MyMapper<ProjectActionRecord> {
-}
