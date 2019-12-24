31ae83c12b62b7f502be1ff0fa4541bd3cbe4f2e
lijing <lijing@diligrp.com>
2019-12-24 10:26:01 +0800

    对象从接口改成对象格式

--- alm/src/main/java/com/dili/alm/domain/Demand.java
+++ alm/src/main/java/com/dili/alm/domain/Demand.java
@@ -1,113 +1,356 @@
 package com.dili.alm.domain;
 
-import com.dili.ss.dto.IBaseDomain;
+import com.dili.ss.domain.BaseDomain;
 import com.dili.ss.metadata.FieldEditor;
 import com.dili.ss.metadata.annotation.EditMode;
 import com.dili.ss.metadata.annotation.FieldDef;
 import java.util.Date;
 import javax.persistence.*;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  * 由MyBatis Generator工具自动生成
  * 
- * This file was generated on 2019-12-23 17:32:14.
+ * This file was generated on 2019-12-24 10:20:34.
  */
 @Table(name = "`demand`")
-public interface Demand extends IBaseDomain {
+public class Demand extends BaseDomain {
+    /**
+     * 自增编号
+     */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "`id`")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 需求编号，区域首字母+年月日+时分+001自增数字
+     */
+    @Column(name = "`serial_number`")
+    private String serialNumber;
+
+    /**
+     * 需求名称
+     */
+    @Column(name = "`name`")
+    private String name;
+
+    /**
+     * 需求申请人
+     */
+    @Column(name = "`user_id`")
+    private Long userId;
+
+    /**
+     * 所属系统
+     */
+    @Column(name = "`belong_pro_id`")
+    private Long belongProId;
+
+    /**
+     * 需求类型（数据字典）
+     */
+    @Column(name = "`type`")
+    private Byte type;
+
+    /**
+     * 需求状态（数据字典）
+     */
+    @Column(name = "`status`")
+    private Byte status;
+
+    /**
+     * 需求期望实现时间(年月日)
+     */
+    @Column(name = "`finish_date`")
+    private Date finishDate;
+
+    /**
+     * 相关附件
+     */
+    @Column(name = "`document_url`")
+    private String documentUrl;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "`create_date`")
+    private Date createDate;
+
+    /**
+     * 提交时间（开启流程）
+     */
+    @Column(name = "`submit_date`")
+    private Date submitDate;
+
+    /**
+     * 需求内容
+     */
+    @Column(name = "`content`")
+    private String content;
+
+    /**
+     * 需求背景
+     */
+    @Column(name = "`reason`")
+    private String reason;
+
+    /**
+     * 获取自增编号
+     *
+     * @return id - 自增编号
+     */
     @FieldDef(label="自增编号")
     @EditMode(editor = FieldEditor.Number, required = true)
-    Long getId();
+    public Long getId() {
+        return id;
+    }
 
-    void setId(Long id);
+    /**
+     * 设置自增编号
+     *
+     * @param id 自增编号
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    @Column(name = "`serial_number`")
+    /**
+     * 获取需求编号，区域首字母+年月日+时分+001自增数字
+     *
+     * @return serial_number - 需求编号，区域首字母+年月日+时分+001自增数字
+     */
     @FieldDef(label="需求编号，区域首字母+年月日+时分+001自增数字", maxLength = 25)
     @EditMode(editor = FieldEditor.Text, required = true)
-    String getSerialNumber();
+    public String getSerialNumber() {
+        return serialNumber;
+    }
 
-    void setSerialNumber(String serialNumber);
+    /**
+     * 设置需求编号，区域首字母+年月日+时分+001自增数字
+     *
+     * @param serialNumber 需求编号，区域首字母+年月日+时分+001自增数字
+     */
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 
-    @Column(name = "`name`")
+    /**
+     * 获取需求名称
+     *
+     * @return name - 需求名称
+     */
     @FieldDef(label="需求名称", maxLength = 25)
     @EditMode(editor = FieldEditor.Text, required = true)
-    String getName();
+    public String getName() {
+        return name;
+    }
 
-    void setName(String name);
+    /**
+     * 设置需求名称
+     *
+     * @param name 需求名称
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-    @Column(name = "`user_id`")
+    /**
+     * 获取需求申请人
+     *
+     * @return user_id - 需求申请人
+     */
     @FieldDef(label="需求申请人")
     @EditMode(editor = FieldEditor.Number, required = true)
-    Long getUserId();
+    public Long getUserId() {
+        return userId;
+    }
 
-    void setUserId(Long userId);
+    /**
+     * 设置需求申请人
+     *
+     * @param userId 需求申请人
+     */
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 
-    @Column(name = "`belong_pro_id`")
+    /**
+     * 获取所属系统
+     *
+     * @return belong_pro_id - 所属系统
+     */
     @FieldDef(label="所属系统")
     @EditMode(editor = FieldEditor.Number, required = false)
-    Long getBelongProId();
+    public Long getBelongProId() {
+        return belongProId;
+    }
 
-    void setBelongProId(Long belongProId);
+    /**
+     * 设置所属系统
+     *
+     * @param belongProId 所属系统
+     */
+    public void setBelongProId(Long belongProId) {
+        this.belongProId = belongProId;
+    }
 
-    @Column(name = "`type`")
+    /**
+     * 获取需求类型（数据字典）
+     *
+     * @return type - 需求类型（数据字典）
+     */
     @FieldDef(label="需求类型（数据字典）")
     @EditMode(editor = FieldEditor.Text, required = true)
-    Byte getType();
+    public Byte getType() {
+        return type;
+    }
 
-    void setType(Byte type);
+    /**
+     * 设置需求类型（数据字典）
+     *
+     * @param type 需求类型（数据字典）
+     */
+    public void setType(Byte type) {
+        this.type = type;
+    }
 
-    @Column(name = "`status`")
+    /**
+     * 获取需求状态（数据字典）
+     *
+     * @return status - 需求状态（数据字典）
+     */
     @FieldDef(label="需求状态（数据字典）")
     @EditMode(editor = FieldEditor.Text, required = true)
-    Byte getStatus();
+    public Byte getStatus() {
+        return status;
+    }
 
-    void setStatus(Byte status);
+    /**
+     * 设置需求状态（数据字典）
+     *
+     * @param status 需求状态（数据字典）
+     */
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
 
-    @Column(name = "`finish_date`")
+    /**
+     * 获取需求期望实现时间(年月日)
+     *
+     * @return finish_date - 需求期望实现时间(年月日)
+     */
     @FieldDef(label="需求期望实现时间(年月日)")
     @EditMode(editor = FieldEditor.Date, required = true)
-    Date getFinishDate();
+    public Date getFinishDate() {
+        return finishDate;
+    }
 
-    void setFinishDate(Date finishDate);
+    /**
+     * 设置需求期望实现时间(年月日)
+     *
+     * @param finishDate 需求期望实现时间(年月日)
+     */
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
 
-    @Column(name = "`document_url`")
+    /**
+     * 获取相关附件
+     *
+     * @return document_url - 相关附件
+     */
     @FieldDef(label="相关附件", maxLength = 120)
     @EditMode(editor = FieldEditor.Text, required = false)
-    String getDocumentUrl();
+    public String getDocumentUrl() {
+        return documentUrl;
+    }
 
-    void setDocumentUrl(String documentUrl);
+    /**
+     * 设置相关附件
+     *
+     * @param documentUrl 相关附件
+     */
+    public void setDocumentUrl(String documentUrl) {
+        this.documentUrl = documentUrl;
+    }
 
-    @Column(name = "`create_date`")
+    /**
+     * 获取创建时间
+     *
+     * @return create_date - 创建时间
+     */
     @FieldDef(label="创建时间")
     @EditMode(editor = FieldEditor.Datetime, required = true)
-    Date getCreateDate();
+    public Date getCreateDate() {
+        return createDate;
+    }
 
-    void setCreateDate(Date createDate);
+    /**
+     * 设置创建时间
+     *
+     * @param createDate 创建时间
+     */
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 
-    @Column(name = "`submit_date`")
+    /**
+     * 获取提交时间（开启流程）
+     *
+     * @return submit_date - 提交时间（开启流程）
+     */
     @FieldDef(label="提交时间（开启流程）")
     @EditMode(editor = FieldEditor.Datetime, required = true)
-    Date getSubmitDate();
+    public Date getSubmitDate() {
+        return submitDate;
+    }
 
-    void setSubmitDate(Date submitDate);
+    /**
+     * 设置提交时间（开启流程）
+     *
+     * @param submitDate 提交时间（开启流程）
+     */
+    public void setSubmitDate(Date submitDate) {
+        this.submitDate = submitDate;
+    }
 
-    @Column(name = "`content`")
+    /**
+     * 获取需求内容
+     *
+     * @return content - 需求内容
+     */
     @FieldDef(label="需求内容")
     @EditMode(editor = FieldEditor.Text, required = true)
-    String getContent();
+    public String getContent() {
+        return content;
+    }
 
-    void setContent(String content);
+    /**
+     * 设置需求内容
+     *
+     * @param content 需求内容
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-    @Column(name = "`reason`")
+    /**
+     * 获取需求背景
+     *
+     * @return reason - 需求背景
+     */
     @FieldDef(label="需求背景")
     @EditMode(editor = FieldEditor.Text, required = false)
-    String getReason();
+    public String getReason() {
+        return reason;
+    }
 
-    void setReason(String reason);
+    /**
+     * 设置需求背景
+     *
+     * @param reason 需求背景
+     */
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
 }
--- alm/src/main/java/com/dili/alm/domain/DemandProject.java
+++ alm/src/main/java/com/dili/alm/domain/DemandProject.java
@@ -1,63 +1,173 @@
 package com.dili.alm.domain;
 
-import com.dili.ss.dto.IBaseDomain;
+import com.dili.ss.domain.BaseDomain;
 import com.dili.ss.metadata.FieldEditor;
 import com.dili.ss.metadata.annotation.EditMode;
 import com.dili.ss.metadata.annotation.FieldDef;
 import javax.persistence.*;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  * 由MyBatis Generator工具自动生成
  * 
- * This file was generated on 2019-12-23 17:47:02.
+ * This file was generated on 2019-12-24 10:21:19.
  */
 @Table(name = "`demand_project`")
-public interface DemandProject extends IBaseDomain {
+public class DemandProject extends BaseDomain {
+    /**
+     * 自增id
+     */
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "`id`")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * 需求ID
+     */
+    @Column(name = "`demand_id`")
+    private Long demandId;
+
+    /**
+     * 关联项目ID
+     */
+    @Column(name = "`project_id`")
+    private Long projectId;
+
+    /**
+     * 关联版本ID
+     */
+    @Column(name = "`version_id`")
+    private Long versionId;
+
+    /**
+     * 需求状态
+     */
+    @Column(name = "`status`")
+    private Long status;
+
+    /**
+     * 关联工单id
+     */
+    @Column(name = "`work_order_id`")
+    private Long workOrderId;
+
+    /**
+     * 获取自增id
+     *
+     * @return id - 自增id
+     */
     @FieldDef(label="自增id")
     @EditMode(editor = FieldEditor.Number, required = true)
-    Long getId();
+    public Long getId() {
+        return id;
+    }
 
-    void setId(Long id);
+    /**
+     * 设置自增id
+     *
+     * @param id 自增id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    @Column(name = "`demand_id`")
+    /**
+     * 获取需求ID
+     *
+     * @return demand_id - 需求ID
+     */
     @FieldDef(label="需求ID")
     @EditMode(editor = FieldEditor.Number, required = true)
-    Long getDemandId();
+    public Long getDemandId() {
+        return demandId;
+    }
 
-    void setDemandId(Long demandId);
+    /**
+     * 设置需求ID
+     *
+     * @param demandId 需求ID
+     */
+    public void setDemandId(Long demandId) {
+        this.demandId = demandId;
+    }
 
-    @Column(name = "`project_id`")
+    /**
+     * 获取关联项目ID
+     *
+     * @return project_id - 关联项目ID
+     */
     @FieldDef(label="关联项目ID")
     @EditMode(editor = FieldEditor.Number, required = false)
-    Long getProjectId();
+    public Long getProjectId() {
+        return projectId;
+    }
 
-    void setProjectId(Long projectId);
+    /**
+     * 设置关联项目ID
+     *
+     * @param projectId 关联项目ID
+     */
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
 
-    @Column(name = "`version_id`")
+    /**
+     * 获取关联版本ID
+     *
+     * @return version_id - 关联版本ID
+     */
     @FieldDef(label="关联版本ID")
     @EditMode(editor = FieldEditor.Number, required = false)
-    Long getVersionId();
+    public Long getVersionId() {
+        return versionId;
+    }
 
-    void setVersionId(Long versionId);
+    /**
+     * 设置关联版本ID
+     *
+     * @param versionId 关联版本ID
+     */
+    public void setVersionId(Long versionId) {
+        this.versionId = versionId;
+    }
 
-    @Column(name = "`status`")
+    /**
+     * 获取需求状态
+     *
+     * @return status - 需求状态
+     */
     @FieldDef(label="需求状态")
     @EditMode(editor = FieldEditor.Number, required = true)
-    Long getStatus();
+    public Long getStatus() {
+        return status;
+    }
 
-    void setStatus(Long status);
+    /**
+     * 设置需求状态
+     *
+     * @param status 需求状态
+     */
+    public void setStatus(Long status) {
+        this.status = status;
+    }
 
-    @Column(name = "`work_order_id`")
+    /**
+     * 获取关联工单id
+     *
+     * @return work_order_id - 关联工单id
+     */
     @FieldDef(label="关联工单id")
     @EditMode(editor = FieldEditor.Number, required = false)
-    Long getWorkOrderId();
+    public Long getWorkOrderId() {
+        return workOrderId;
+    }
 
-    void setWorkOrderId(Long workOrderId);
+    /**
+     * 设置关联工单id
+     *
+     * @param workOrderId 关联工单id
+     */
+    public void setWorkOrderId(Long workOrderId) {
+        this.workOrderId = workOrderId;
+    }
 }
