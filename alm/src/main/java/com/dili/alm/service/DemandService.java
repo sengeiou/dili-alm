bacffab1cf4670c29fdf490c6f9098f29a1c6ad4
hanjinzhao <hjz1573688@126.com>
2019-12-26 11:18:17 +0800

    添加需求详情页面

--- alm/src/main/java/com/dili/alm/service/DemandService.java
+++ alm/src/main/java/com/dili/alm/service/DemandService.java
@@ -58,33 +58,5 @@ public interface DemandService extends BaseService<Demand, Long> {
 
 	DemandDto getDetailViewData(Long id) throws Exception;
 
-	static Object parseViewModel(DemandDto detailViewData) throws Exception {
-		Map<Object, Object> metadata = new HashMap<>();
-
-		JSONObject demandProjectProvider = new JSONObject();
-		demandProjectProvider.put("provider", "demandProjectProvider");
-		metadata.put("belongProId", demandProjectProvider);
-
-		JSONObject memberProvider = new JSONObject();
-		memberProvider.put("provider", "memberProvider");
-		metadata.put("userId", memberProvider);
-
-		JSONObject demandStatusProvider = new JSONObject();
-		demandStatusProvider.put("provider", "demandStatusProvider");
-		metadata.put("status", demandStatusProvider);
-
-		JSONObject demandTypeProvider = new JSONObject();
-		demandTypeProvider.put("provider", "demandTypeProvider");
-		metadata.put("type", demandTypeProvider);
-
-		JSONObject datetimeProvider = new JSONObject();
-		datetimeProvider.put("provider", "datetimeProvider");
-		metadata.put("createDate", datetimeProvider);
-		metadata.put("submitDate", datetimeProvider);
-		metadata.put("finishDate", datetimeProvider);
-
-		List<Map> listMap = ValueProviderUtils.buildDataByProvider(metadata,
-				Arrays.asList(detailViewData));
-		return listMap.get(0);
-	}
+	
 }
