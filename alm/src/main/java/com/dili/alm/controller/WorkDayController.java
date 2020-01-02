b71d462a2b9b796fa008bc50d270d04c9998c4ea
hanjinzhao <hjz1573688@126.com>
2020-01-02 16:09:30 +0800

    添加注释

--- alm/src/main/java/com/dili/alm/controller/WorkDayController.java
+++ alm/src/main/java/com/dili/alm/controller/WorkDayController.java
@@ -1,10 +1,12 @@
 package com.dili.alm.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.dili.alm.domain.dto.WorkDayRoleDto;
 import com.dili.alm.service.WorkDayService;
 import com.dili.alm.utils.DateUtil;
+import com.dili.alm.utils.WebUtil;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.uap.sdk.domain.UserTicket;
 import com.dili.uap.sdk.session.SessionContext;
@@ -38,6 +41,10 @@ public class WorkDayController {
     @ApiImplicitParams({
 		@ApiImplicitParam(name="WorkDay", paramType="form", value = "WorkDay的form信息", required = true, dataType = "string")
 	})
+    /**
+     * 判断是不是最后一天
+     * @return
+     */
     @ResponseBody
     @RequestMapping(value="/isWorkEndDayDate", method = {RequestMethod.GET, RequestMethod.POST})
     public int isWorkEndDayDate() {
@@ -61,13 +68,64 @@ public class WorkDayController {
        return 0;
     }
     
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="WorkDay", paramType="form", value = "WorkDay的form信息", required = true, dataType = "string")
-	})
-    @RequestMapping(value="/uploadWordDayDate", method = {RequestMethod.GET, RequestMethod.POST})
-    public  @ResponseBody BaseOutput  uploadWordDayDate(@RequestParam("file") MultipartFile file,String year) {
-    	return workDayService.upload(file,year);	
-    }
     
-    
+	/**
+	 * 上传ecxel解析导入mysql
+	 * 
+	 * @param file
+	 * @param year
+	 * @return
+	 */
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "WorkDay", paramType = "form", value = "WorkDay的form信息", required = true, dataType = "string") })
+	@RequestMapping(value = "/uploadWordDayDate", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody BaseOutput uploadWordDayDate(@RequestParam("file") MultipartFile file, String year) {
+		return workDayService.upload(file, year);
+	}
+
+	/**
+	 * 查询导入的所有年份
+	 * 
+	 * @return
+	 */
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "WorkDay", paramType = "form", value = "WorkDay的form信息", required = true, dataType = "string") })
+	@RequestMapping(value = "/selectWorkDayYear", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody List<String> selectWorkDayYear() {
+		return workDayService.getWorkDayYaers();
+	}
+
+	/**
+	 * 查询当前工作时间
+	 * 
+	 * @return
+	 */
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "WorkDay", paramType = "form", value = "WorkDay的form信息", required = true, dataType = "string") })
+	@RequestMapping(value = "/getWorkDay", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody BaseOutput<WorkDayRoleDto> getWorkDay(String userId) {
+		if (WebUtil.strIsEmpty(userId)) {
+			throw new RuntimeException("未登录");
+		}
+		BaseOutput<WorkDayRoleDto> showWorkDay = workDayService.showWorkDay(Long.parseLong(userId));
+		if ("false".equals(showWorkDay.getCode())) {
+			return BaseOutput.failure("需要导入新的工作日").setData(showWorkDay.getData());
+		} else {
+			return BaseOutput.success().setData(showWorkDay.getData());
+		}
+
+	}
+
+	/**
+	 * 查询当前年份
+	 * 
+	 * @return
+	 */
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "WorkDay", paramType = "form", value = "WorkDay的form信息", required = true, dataType = "string") })
+	@RequestMapping(value = "/isHasWorkDayYear", method = { RequestMethod.GET, RequestMethod.POST })
+	public @ResponseBody boolean isHasWorkDayYear(String year) {
+		List<String> workDayYaers = workDayService.getWorkDayYaers();
+		return workDayYaers.contains(year);
+	}
 }
