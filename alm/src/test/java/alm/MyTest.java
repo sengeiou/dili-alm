e6b64da265ef714c64e4ae8f23f44e845624ee20
jiang <jiang@DESKTOP-GJ4LO3T>
2020-02-13 19:41:54 +0800

    更新maven依赖，及利刃框架版本

--- alm/src/test/java/alm/MyTest.java
+++ alm/src/test/java/alm/MyTest.java
@@ -1,5 +1,7 @@
 package alm;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,10 +9,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.dili.AlmApplication;
+import com.dili.alm.domain.OnlineDataChange;
 import com.dili.alm.domain.ProjectOnlineApply;
+import com.dili.alm.domain.dto.OnlineDataChangeBpmcDtoDto;
+import com.dili.alm.service.OnlineDataChangeService;
 import com.dili.alm.service.ProjectOnlineApplyService;
 import com.dili.bpmc.sdk.rpc.FormRpc;
 import com.dili.bpmc.sdk.rpc.TaskRpc;
+import com.dili.ss.util.BeanConver;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = AlmApplication.class)
@@ -22,11 +28,14 @@ public class MyTest {
 	private TaskRpc taskRpc;
 	@Autowired
 	private ProjectOnlineApplyService projectOnlineApplyService;
+	@Autowired
+	private OnlineDataChangeService onlineDataChangeService;
 
 	@Test
 	public void test() {
-		ProjectOnlineApply apply = this.projectOnlineApplyService.getBySerialNumber("2002044801");
-		System.out.println(apply.getExecutorId());
+		List<OnlineDataChange> list = this.onlineDataChangeService.list(new OnlineDataChange());
+		List<OnlineDataChangeBpmcDtoDto> targetList = BeanConver.copyList(list, OnlineDataChangeBpmcDtoDto.class);
+		System.out.println(targetList);
 	}
 
 }
