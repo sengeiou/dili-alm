07c91247aff8561fe653cc6b23e03575a2017d3e
lijing <dili167@DESKTOP-5UVHRSB>
2020-06-30 15:15:31 +0800

    修改部门负责人审批为运维组审批，审批对象为候选组

--- alm/src/main/java/com/dili/alm/domain/DemandOperationType.java
+++ alm/src/main/java/com/dili/alm/domain/DemandOperationType.java
@@ -2,7 +2,7 @@ package com.dili.alm.domain;
 
 public enum DemandOperationType {
 
-	DEMAND_MANAGER(1, "部门经理审批"), ACCEPT(2, "研发接收需求"), ASSIGN(3, "指定责任人"), RECIPROCATE(4, "责任人对接"),FEEDBACK(5,"记录反馈方案"),DEMAND_ADMIN(6,"需求管理员"),REJECT(7,"需求被驳回"),RESUBMIT(8,"重新提交");
+	DEMAND_MANAGER(1, "运维审批"), ACCEPT(2, "研发接收需求"), ASSIGN(3, "指定责任人"), RECIPROCATE(4, "责任人对接"),FEEDBACK(5,"记录反馈方案"),DEMAND_ADMIN(6,"需求管理员"),REJECT(7,"需求被驳回"),RESUBMIT(8,"重新提交");
 
 	private Integer value;
 	private String name;
