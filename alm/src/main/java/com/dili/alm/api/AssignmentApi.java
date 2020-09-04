e42c0fce66afe169179186f9e981d02cf296c95d
jiang <jiang@DESKTOP-GJ4LO3T>
2020-09-04 17:37:42 +0800

    解决流控中心调用api报错的问题

--- alm/src/main/java/com/dili/alm/api/AssignmentApi.java
+++ alm/src/main/java/com/dili/alm/api/AssignmentApi.java
@@ -1,7 +1,5 @@
 package com.dili.alm.api;
 
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,7 +33,7 @@ public class AssignmentApi {
 	// 申请数据变
 //	@ApiOperation("申请数据变")
 	@PostMapping("/setSubmitOnlineDataChangeAssigneeName.api")
-	public BaseOutput<Assignment> setsubmitOnlineDataChangeAssigneeName( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> setsubmitOnlineDataChangeAssigneeName(TaskMapping taskMapping) {
 		String dept = taskMapping.getProcessVariables().get(BpmConsts.OnlineDataChangeProcessConstant.submit.getName()).toString();
 		Assignment assignment = DTOUtils.newInstance(Assignment.class);
 		assignment.setAssignee(dept);
@@ -43,9 +41,9 @@ public class AssignmentApi {
 	}
 
 	// 根据变更ID，返回部门审批人
-/*	@ApiOperation("根据变更ID，返回部门审批人")*/
+	/* @ApiOperation("根据变更ID，返回部门审批人") */
 	@PostMapping("/setDeptOnlineDataChangeAssigneeName.api")
-	public BaseOutput<Assignment> setDeptOnlineDataChangeAssigneeName( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> setDeptOnlineDataChangeAssigneeName(TaskMapping taskMapping) {
 		String dept = taskMapping.getProcessVariables().get(BpmConsts.OnlineDataChangeProcessConstant.dept.getName()).toString();
 		Assignment assignment = DTOUtils.newInstance(Assignment.class);
 		assignment.setAssignee(dept);
@@ -53,9 +51,9 @@ public class AssignmentApi {
 	}
 
 	// 根据变更ID，返回测试审批人
-/*	@ApiOperation("根据变更ID，返回测试审批人")*/
+	/* @ApiOperation("根据变更ID，返回测试审批人") */
 	@PostMapping("/setTestOnlineDataChangeAssigneeName.api")
-	public BaseOutput<Assignment> setTestOnlineDataChangeAssigneeName( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> setTestOnlineDataChangeAssigneeName(TaskMapping taskMapping) {
 		String dept = taskMapping.getProcessVariables().get(BpmConsts.OnlineDataChangeProcessConstant.test.getName()).toString();
 		Assignment assignment = DTOUtils.newInstance(Assignment.class);
 		assignment.setAssignee(dept);
@@ -81,8 +79,7 @@ public class AssignmentApi {
 	 * @ApiOperation("根据变更ID，返回dba审批人")
 	 * 
 	 * @RequestMapping("/setOnlineConfirm") public @ResponseBody
-	 * BaseOutput<Assignment> setOnlineConfirm( Long onlineDataChangeId)
-	 * {
+	 * BaseOutput<Assignment> setOnlineConfirm( Long onlineDataChangeId) {
 	 * 
 	 * Assignment record = assignmentService.setOnlineConfirm(onlineDataChangeId);
 	 * return BaseOutput.success().setData(record);
@@ -91,43 +88,47 @@ public class AssignmentApi {
 	 */
 
 	// 根据变更ID，返回测试审批人
-/*	@ApiOperation("根据变更ID,項目申請人")*/
+	/* @ApiOperation("根据变更ID,項目申請人") */
 	@PostMapping("/getOnlineDataChangeUserName.api")
-	public BaseOutput<String> getOnlineDataChangeUserName( TaskMapping taskMapping) {
+	public BaseOutput<String> getOnlineDataChangeUserName(TaskMapping taskMapping) {
 		String dept = taskMapping.getProcessVariables().get("apply").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(dept);
 		return BaseOutput.success().setData(assignment);
 	}
 
-    /****需求管理 begin*****/
-/*	@ApiOperation("获取需求部门经理审批人")*/
+	/**** 需求管理 begin *****/
+	/* @ApiOperation("获取需求部门经理审批人") */
 	@PostMapping("/setDepartmentManagerId.api")
-	public BaseOutput<String> setDepartmentManagerId( TaskMapping taskMapping) {
+	public BaseOutput<String> setDepartmentManagerId(TaskMapping taskMapping) {
 		String executorId = taskMapping.getProcessVariables().get("departmentManagerId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
 		return BaseOutput.success().setData(assignment);
 	}
-/*	@ApiOperation("根据需求ID,需求反馈人")*/
+
+	/* @ApiOperation("根据需求ID,需求反馈人") */
 	@PostMapping("/setFeedback.api")
-	public BaseOutput<String> setFeedback( TaskMapping taskMapping) {
-/*		Map<String, Object> map = taskMapping.getProcessVariables();
-		Assignment record = assignmentService.setFeedback(map.get("businessKey").toString());
-		return BaseOutput.success().setData(record);*/
+	public BaseOutput<String> setFeedback(TaskMapping taskMapping) {
+		/*
+		 * Map<String, Object> map = taskMapping.getProcessVariables(); Assignment
+		 * record = assignmentService.setFeedback(map.get("businessKey").toString());
+		 * return BaseOutput.success().setData(record);
+		 */
 		String executorId = taskMapping.getProcessVariables().get("AssignExecutorId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
 		return BaseOutput.success().setData(assignment);
 	}
 
-	
-/*	@ApiOperation("根据变更ID,需求处理人")*/
+	/* @ApiOperation("根据变更ID,需求处理人") */
 	@PostMapping("/setReciprocate.api")
-	public BaseOutput<String> setReciprocate( TaskMapping taskMapping) {
-/*		Map<String, Object> map = taskMapping.getProcessVariables();
-		Assignment record = assignmentService.setReciprocate(map.get("businessKey").toString());
-		return BaseOutput.success().setData(record);*/
+	public BaseOutput<String> setReciprocate(TaskMapping taskMapping) {
+		/*
+		 * Map<String, Object> map = taskMapping.getProcessVariables(); Assignment
+		 * record = assignmentService.setReciprocate(map.get("businessKey").toString());
+		 * return BaseOutput.success().setData(record);
+		 */
 		String executorId = taskMapping.getProcessVariables().get("AssignExecutorId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
@@ -136,60 +137,73 @@ public class AssignmentApi {
 	}
 
 	// 驳回返回给需求发布人
-/*	@ApiOperation("根据变更ID,項目申請人")*/
+	/* @ApiOperation("根据变更ID,項目申請人") */
 	@PostMapping("/setDemandAppId.api")
-	public BaseOutput<String> setDemandAppId( TaskMapping taskMapping) {
-/*		Map<String, Object> map = taskMapping.getProcessVariables();
-		Assignment record = assignmentService.setDemandAppId(map.get("businessKey").toString());
-		return BaseOutput.success().setData(record);*/
+	public BaseOutput<String> setDemandAppId(TaskMapping taskMapping) {
+		/*
+		 * Map<String, Object> map = taskMapping.getProcessVariables(); Assignment
+		 * record = assignmentService.setDemandAppId(map.get("businessKey").toString());
+		 * return BaseOutput.success().setData(record);
+		 */
 		String executorId = taskMapping.getProcessVariables().get("AssignExecutorId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
 		return BaseOutput.success().setData(assignment);
 	}
-	 /****需求管理 end*****/
-	/**LJ add begin**/
+
+	/**** 需求管理 end *****/
+	/** LJ add begin **/
 	// 资源申请，设置项目经理
-/*	@ApiOperation("项目id,设置项目经理")*/
+	/* @ApiOperation("项目id,设置项目经理") */
 	@PostMapping("/setHardwareApplyProjectManager.api")
-	public BaseOutput<String> setHardwareApplyProjectManager( TaskMapping taskMapping) {
-/*		Map<String, Object> map = taskMapping.getProcessVariables();
-		Assignment record = assignmentService.setProjectManager(map.get("businessKey").toString());
-		return BaseOutput.success().setData(record);*/
+	public BaseOutput<String> setHardwareApplyProjectManager(TaskMapping taskMapping) {
+		/*
+		 * Map<String, Object> map = taskMapping.getProcessVariables(); Assignment
+		 * record =
+		 * assignmentService.setProjectManager(map.get("businessKey").toString());
+		 * return BaseOutput.success().setData(record);
+		 */
 		String executorId = taskMapping.getProcessVariables().get("AssignExecutorId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
 		return BaseOutput.success().setData(assignment);
 	}
-	
+
 	// 资源申请，设置部署运维人员
-/*	@ApiOperation("根据页面保存的运维操作人员")*/
+	/* @ApiOperation("根据页面保存的运维操作人员") */
 	@PostMapping("/setHardwareApplyOpdrator.api")
-	public BaseOutput<String> setHardwareApplyOpdrator( TaskMapping taskMapping) {
-/*		Map<String, Object> map = taskMapping.getProcessVariables();
-		Assignment record = assignmentService.setOpdrator(map.get("businessKey").toString());
-		return BaseOutput.success().setData(record);*/
+	public BaseOutput<String> setHardwareApplyOpdrator(TaskMapping taskMapping) {
+		/*
+		 * Map<String, Object> map = taskMapping.getProcessVariables(); Assignment
+		 * record = assignmentService.setOpdrator(map.get("businessKey").toString());
+		 * return BaseOutput.success().setData(record);
+		 */
 		String executorId = taskMapping.getProcessVariables().get("AssignExecutorId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
 		return BaseOutput.success().setData(assignment);
 	}
+
 	// 资源申请，设置返回编辑
-/*	@ApiOperation("返回编辑者")*/
+	/* @ApiOperation("返回编辑者") */
 	@RequestMapping("/setHardwareApplyApplicant.api")
-	public BaseOutput<String> setHardwareApplyApplicant( TaskMapping taskMapping) {
-/*		Map<String, Object> map = taskMapping.getProcessVariables();
-		Assignment record = assignmentService.setHardwareResourceApplyApply(map.get("businessKey").toString());
-		return BaseOutput.success().setData(record);*/
+	public BaseOutput<String> setHardwareApplyApplicant(TaskMapping taskMapping) {
+		/*
+		 * Map<String, Object> map = taskMapping.getProcessVariables(); Assignment
+		 * record =
+		 * assignmentService.setHardwareResourceApplyApply(map.get("businessKey").
+		 * toString()); return BaseOutput.success().setData(record);
+		 */
 		String executorId = taskMapping.getProcessVariables().get("AssignExecutorId").toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
 		return BaseOutput.success().setData(assignment);
 	}
-	/**LJ add end**/
+
+	/** LJ add end **/
 	@ResponseBody
 	@RequestMapping(value = "/getProjectOnlineApplyProjectManager.api")
-	public BaseOutput<Assignment> getProjectManager( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> getProjectManager(TaskMapping taskMapping) {
 		String projectManagerId = taskMapping.getProcessVariables().get(BpmConsts.ProjectOnlineApplyConstant.PROJECT_MANAGER_KEY.getName()).toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(projectManagerId);
@@ -198,7 +212,7 @@ public class AssignmentApi {
 
 	@ResponseBody
 	@RequestMapping(value = "/getProjectOnlineApplyExecutor.api")
-	public BaseOutput<Assignment> getProjectOnlineApplyExecutor( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> getProjectOnlineApplyExecutor(TaskMapping taskMapping) {
 		String executorId = taskMapping.getProcessVariables().get(BpmConsts.ProjectOnlineApplyConstant.EXECUTOR_KEY.getName()).toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(executorId);
@@ -207,16 +221,16 @@ public class AssignmentApi {
 
 	@ResponseBody
 	@RequestMapping(value = "/getProjectOnlineApplyProductManager.api")
-	public BaseOutput<Assignment> getProjectOnlineApplyProductManager( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> getProjectOnlineApplyProductManager(TaskMapping taskMapping) {
 		String productManagerId = taskMapping.getProcessVariables().get(BpmConsts.ProjectOnlineApplyConstant.PRODUCT_MANAGER_KEY.getName()).toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(productManagerId);
 		return BaseOutput.success().setData(assignment);
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/getProjectOnlineApplyApplicant.api")
-	public BaseOutput<Assignment> getProjectOnlineApplyApplicant( TaskMapping taskMapping) {
+	public BaseOutput<Assignment> getProjectOnlineApplyApplicant(TaskMapping taskMapping) {
 		String applicantId = taskMapping.getProcessVariables().get(BpmConsts.ProjectOnlineApplyConstant.APPLICANT_KEY.getName()).toString();
 		Assignment assignment = DTOUtils.newDTO(Assignment.class);
 		assignment.setAssignee(applicantId);
