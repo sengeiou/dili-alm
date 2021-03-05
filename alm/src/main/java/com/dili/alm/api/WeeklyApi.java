e533c0d389cc31cd62ccdc2befbd59949c3d55b5
yangfan <yangfan>
2021-03-05 14:23:48 +0800

    注释

--- alm/src/main/java/com/dili/alm/api/WeeklyApi.java
+++ alm/src/main/java/com/dili/alm/api/WeeklyApi.java
@@ -9,6 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dili.alm.service.WeeklyService;
 import com.dili.ss.domain.BaseOutput;
 
+/**
+ * 
+ * <B>Description</B>
+ * 本软件源代码版权归农丰时代及其团队所有,未经许可不得任意复制与传播
+ * <B>农丰时代科技有限公司</B>
+ *
+ * @Description TODO(用一句话描述该文件做什么)
+ * @author yangfan
+ * @date 2021年3月5日
+ */
 @RequestMapping("/weeklyApi")
 @RestController
 public class WeeklyApi {
--- alm/src/main/java/com/dili/alm/service/AssignmentService.java
+++ alm/src/main/java/com/dili/alm/service/AssignmentService.java
@@ -4,6 +4,16 @@ import java.util.List;
 
 import com.dili.bpmc.sdk.dto.Assignment;
 
+/**
+ * 
+ * <B>Description</B>
+ * 本软件源代码版权归农丰时代及其团队所有,未经许可不得任意复制与传播
+ * <B>农丰时代科技有限公司</B>
+ *
+ * @Description TODO(用一句话描述该文件做什么)
+ * @author yangfan
+ * @date 2021年3月5日
+ */
 public interface AssignmentService {
 	
 	Assignment setSubmitOnlineDataChangeAssigneeName(Long onlineDataChangeId);
